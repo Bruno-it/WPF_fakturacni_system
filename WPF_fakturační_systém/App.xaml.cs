3725313bd098ccd05603007d7bcc64f09f3fb15d
Bruno-it <125484917+Bruno-it@users.noreply.github.com>
2025-11-25 11:59:32 +0100

    Remove summary comments from App.xaml.cs
    Removed XML summary comments from App.xaml.cs

--- WPF_fakturační_systém/App.xaml.cs
+++ WPF_fakturační_systém/App.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace WPF_fakturační_systém
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
